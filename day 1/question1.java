359a5bd8d9ce477d477caa4f5aab40dc7df02402
vanshcreates19 <gargvansh1901@gmail.com>
2026-06-02 21:40:19 +0530

    added day 1 and day 2 assignments

---
+++ day 1/question1.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class question1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of N: ");
+        int n = sc.nextInt();
+
+        int sum = 0;
+
+        for (int i = 1; i <= n; i++) {
+            sum += i;
+        }
+
+        System.out.println("Sum of first " + n + " natural numbers = " + sum);
+
+        sc.close();
+    }
+}
---
+++ day 1/question4.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class question4 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        int count = 0;
+
+        while (num != 0) {
+            num = num / 10;
+            count++;
+        }
+
+        System.out.println("Number of digits = " + count);
+
+        sc.close();
+    }
+}
+    
+
